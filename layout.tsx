071d7ed18bd98d9b7bcdd07d3787c56cba709844
ialyramx-creator <ialyramx@gmail.com>
2026-02-27 00:00:43 -0600

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Star Healthy Studio Fit',
+  description: 'El movimiento no es una opcion, es un estilo de vida. Únete a nuestra comunidad.',
+  generator: 'v0.dev',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="es">
+      <head>
+        <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistMono.variable};
+}
+        `}</style>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
